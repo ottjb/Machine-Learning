1675e769e4615ed534d721b624d324bded32644a
ottjb <ottjb@usca.edu>
2024-03-13 12:07:59 -0400

    Snake v1

--- Neural Networks/Snake.py
+++ Neural Networks/Snake.py
@@ -10,22 +10,22 @@ from pygame.locals import (
     QUIT
 )
 
-INPUT_SIZE = 28
-HIDDEN1_SIZE = 16
-HIDDEN2_SIZE = 16
-OUTPUT_SIZE = 4
-POPULATION_SIZE = 100
-MUTATION_RATE = 0.01
-
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 200
+SCREEN_HEIGHT = 200
 BLOCK_SIZE = 20
+col = SCREEN_WIDTH // BLOCK_SIZE
+row = SCREEN_HEIGHT // BLOCK_SIZE
 FRAMES_PER_SECOND = 10
-col = SCREEN_WIDTH / BLOCK_SIZE
-row = SCREEN_HEIGHT / BLOCK_SIZE
 
 snake = None
 food = None
+
+INPUT_SIZE = 7
+HIDDEN1_SIZE = 16
+HIDDEN2_SIZE = 16
+OUTPUT_SIZE = 4
+POPULATION_SIZE = 5
+MUTATION_RATE = 0.01
 
 # Define colors
 red = (255, 0, 0)
@@ -40,7 +40,7 @@ class Snake():
     def __init__(self, color=green):
         self.length = 1
         self.points = [((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))]
-        self.direction = (0, 0)
+        self.direction = (1, 0)
         self.color = color
         self.alive = True
         self.weights_ih = np.random.rand(INPUT_SIZE, HIDDEN1_SIZE)
@@ -49,6 +49,9 @@ class Snake():
         
     def getHeadPosition(self):
         return self.points[0]
+    
+    def getDirection(self):
+        return self.direction
         
     def turn(self, x, y):
         self.direction = (x, y)
@@ -75,18 +78,124 @@ class Snake():
         self.hidden2_activation = self.sigmoid(self.hidden2)
         
         self.output = np.dot(self.hidden2_activation, self.weights_ho)
-        self.output_activation = self.sigmoid(self.output)
+        self.output_activation = self.softmax(self.output)
         return self.output_activation
     
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
     
+    def relu(self, x):
+        return np.maximum(0, x)
+    
+    def softmax(self, x):
+        exp_values = np.exp(x - np.max(x, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, keepdims=True)
+        return probabilities
+    
     # for i in range to check a certain distance in each direction
     def gatherInputs(self, snakeHeadPosition, foodPosition):
-        inputs = []
-        for dx, dy in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-            inputs.append(int((foodPosition[0] + dx * BLOCK_SIZE, foodPosition[1] + dy * BLOCK_SIZE) == snakeHeadPosition))
-        return inputs
+        '''
+        foodInputs = []
+        wallInputs = []
+        bodyInputs = []
+        directionInputs = []
+        vision = 50
+        wallVision = 3
+        foodInVision = False
+        wallInVision = False
+        bodyInVision = False
+        for dx, dy in [(0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1)]:
+            for i in range(1, vision + 1):
+                x = snakeHeadPosition[0] + dx * i * BLOCK_SIZE
+                y = snakeHeadPosition[1] + dy * i * BLOCK_SIZE
+                if (x, y) == foodPosition:
+                    foodInVision = True
+                if (x, y) in self.points:
+                    bodyInVision = True
+
+            for i in range(1, wallVision + 1):
+                x = snakeHeadPosition[0] + dx * i * BLOCK_SIZE
+                y = snakeHeadPosition[1] + dy * i * BLOCK_SIZE
+                if x < 0 or x >= SCREEN_WIDTH or y < 0 or y >= SCREEN_HEIGHT:
+                    wallInVision = True
+
+            if foodInVision:
+                foodInputs.append(1)
+                foodInVision = False
+            else:
+                foodInputs.append(0)
+
+            if wallInVision:
+                wallInputs.append(1)
+                wallInVision = False
+            else:
+                wallInputs.append(0)
+            
+            if bodyInVision:
+                bodyInputs.append(1)
+                bodyInVision = False
+            else:
+                bodyInputs.append(0)
+
+        for dx, dy in [(1, 0), (-1, 0), (0, -1), (0, 1)]:
+            if (dx, dy) == self.direction:
+                directionInputs.append(1)
+            else:
+                directionInputs.append(0)
+            
+        return foodInputs + wallInputs + bodyInputs + directionInputs
+        '''
+        
+
+        distanceFromWalls = [snakeHeadPosition[0] // BLOCK_SIZE, SCREEN_WIDTH - snakeHeadPosition[0], snakeHeadPosition[1], SCREEN_HEIGHT - snakeHeadPosition[1]]
+
+        foodVector_x = foodPosition[0] - snakeHeadPosition[0]
+        foodVector_y = foodPosition[1] - snakeHeadPosition[1]
+        directionOfFood = np.degrees(np.arctan2(foodVector_y, foodVector_x))
+        if directionOfFood < 0:
+            directionOfFood += 360
+
+        # Get 2d array of the snake's body
+        # body = np.zeros((int(SCREEN_WIDTH / BLOCK_SIZE), int(SCREEN_HEIGHT / BLOCK_SIZE)))
+        # for i in range(col):
+        #     for j in range(row):
+        #         x = i * BLOCK_SIZE
+        #         y = j * BLOCK_SIZE
+        #         if (x, y) == self.getHeadPosition():
+        #             body[i][j] = 2
+        #         elif (x, y) in self.points:
+        #             body[i][j] = 1
+        #         elif (x, y) == foodPosition:
+        #             body[i][j] = 3
+        #         else:
+        #             body[i][j] = 0
+        
+        # Direction of the snake
+        direction = None
+        if self.direction == (-1, 0):
+            direction = 0
+        elif self.direction == (1, 0):
+            direction = 1
+        elif self.direction == (0, -1):
+            direction = 2
+        elif self.direction == (0, 1):
+            direction = 3
+
+        proximityToTail = 0
+        for point in self.points:
+            distance = abs(snakeHeadPosition[0] - point[0]) + abs(snakeHeadPosition[1] - point[1])
+            proximityToTail = min(proximityToTail, distance)
+
+        input = distanceFromWalls + [directionOfFood]
+        # for i in range(col):
+        #     for j in range(row):
+        #         input.append(body[i][j])
+        input.append(direction)
+        input.append(proximityToTail)
+        return input
+        
+
+        
     
     
 class Food():
@@ -111,12 +220,26 @@ def runGame(snake : Snake):
     clock = pg.time.Clock()
     snake = snake
     food = Food(snake.points)
-    print(snake.getHeadPosition(), food.position)
     lastDirection = (0, 0)
-    direction = (0, 0)
+    direction = snake.getDirection()
+    framesAlive = 0
     # Game Loop
     running = True
     while running:
+        inputs = snake.gatherInputs(snake.getHeadPosition(), food.position)
+        outputs = snake.feedforward(inputs)
+        print(inputs)
+        print(outputs)
+        max_output_index = np.argmax(outputs)
+        # Convert the index to a direction
+        if max_output_index == 0:
+            direction = (-1, 0)
+        elif max_output_index == 1:
+            direction = (1, 0)
+        elif max_output_index == 2:
+            direction = (0, -1)
+        elif max_output_index == 3:
+            direction = (0, 1)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
@@ -137,21 +260,20 @@ def runGame(snake : Snake):
             snake.points.append((food.position[0], food.position[1]))
             food = Food(snake.points)
         if not snake.alive:
-            running = False
-            
-        inputs = snake.gatherInputs(snake.getHeadPosition(), food.position)
-        print(inputs)
+            return (framesAlive, snake.length - 1)
 
         screen.fill(black)
         snake.draw(screen)
         food.draw(screen)
         pg.display.update()
+        framesAlive += 1
         clock.tick(FRAMES_PER_SECOND)
         
 def main():
-    snake = Snake()
-    
-    runGame(snake)
+    for i in range(POPULATION_SIZE):
+        snake = Snake()
+        runGame(snake)
+
         
 if __name__ == "__main__":
     main()
